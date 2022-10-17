f0dd8745556ae2118ffe1d1e3d0a1ac55327799d
RASARUS <seonbi1998@naver.com>
2022-10-17 23:37:39 +0900

    new

---
+++ todo/src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <hr />
+    <input type="checkbox" />
+    <p>input에서 받은 데이터</p>
+    <button>X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+};
+</script>
+
+<style>
+</style>
