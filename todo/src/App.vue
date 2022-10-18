99a59c839465444427047d969ec2f2fbf6e4585e
Jeong Jiun <jujeong@ncurity.com>
2022-10-18 19:02:40 +0900

    todo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -3,22 +3,19 @@
     <div id="main">
       <div>
         <h4 id="title">To Do List</h4>
-        <input
-          v-model="Input"
-          class="input"
-          placeholder="할일을 입력해 주세요"
-        />
-        <!-- 버튼누르거나 엔터치면 추가 -->
-        <button class="addButton">+</button>
+        <input type="text" v-model="Inputs" @keyup.enter="setData" class="input" placeholder="할일을 입력해 주세요">
+        <button :value="Inputs" @click="setData" class="addButton">+</button>
       </div>
-      <List />
+      <hr style="border:1px dashed black" />
+      <List :loadData="item[index]" v-for="(item, index) in loadData()" @tog="toggleComplete(item[index])"
+        :key="index" />
     </div>
   </div>
-</template>
+</template> 
 
 <script>
-//input 부분을 컴포넌트로 만들고 데이터를 props로 보낼때의 이점보다 데이터를 주고받을때의 복잡함 등 단점이 더 크다고 판단합니다.
-//props로 보내면 자식컴포넌트에서는 Read-Only가 되기때문에 일반적으로 변경이 되지않고, 설령 Custom Event를 사용하면 되지만 코드가 더 복잡해지는 경우가 생겨 input은 컴포넌트로 사용하지 않았습니다.
+//input부분을 컴포넌트로 만들고 이점보다 데이터를 주고받을때의 복잡함 등 단점이 더 크다고 판단합니다.
+//props로 보내면 자식컴포넌트에서는 Read-Only가 되기때문에 일반적으로 변경이 되지않고, 설령 Custom Event를 사용하더라도 코드가 더 복잡해지는 경우가 생겨 input부분은 컴포넌트로 사용하지 않았습니다.
 //컴포넌트는 반복적인 구문이 올때 효과적이며 간결하게 보일수 있지만, '한번만 쓰이고 데이터를 양방향으로 보내는 블럭'에 사용하기에는 오히려 복잡해 집니다.(강의보고 조금더 보충하자)
 import List from "./components/List.vue";
 
@@ -26,12 +23,56 @@ export default {
   name: "App",
   data() {
     return {
-      Input: "",
+      Inputs: "",
+      todoList: [],
+      get: [],
     };
   },
   components: {
     List,
   },
+  methods: {
+    loadData: function () {
+      if (localStorage.getItem("list")) {
+        this.get = JSON.parse(localStorage.getItem("list"))
+        return this.get
+      }
+    },
+    setData: function () {
+      if (this.Inputs == "") {
+        alert("할일을 입력하세요.")
+        return
+      }
+      this.addData(this.Inputs);
+      this.Inputs = ""
+    },
+    addData: function (inputText) { //새로고침후 입력시 LocalStorage에 덮어써지는 문제 해결
+      if (this.get.length > 0) { //get에 데이터가있으면 get에푸쉬
+        this.get.push({ id: new Date().toISOString(), data: inputText, complete: false })
+        localStorage.setItem("list", JSON.stringify(this.get))
+      }
+      else {
+        this.todoList.push({ id: new Date().toISOString(), data: inputText, complete: false })
+        localStorage.setItem("list", JSON.stringify(this.todoList))
+      }
+    },
+    toggleComplete: function (todo) {
+      todo.complete = todo.complete === 'false' ? 'true' : 'false'
+    },
+    // isChecked: function (id) {
+    //   // const p = document.querySelector(".inputData");
+    //   const target = this.loadData().filter(D => D.id == id)[0]; //render에서 받은 파라미터와 todos의 요소를 비교함
+    //   target.complete = !target.complete;
+    //   localStorage.setItem("list", JSON.stringify(this.get));
+    // },
+    // initialize: function () {
+    //   const p = document.querySelector(".inputData");
+    //   this.get.map(got => {
+    //     if (got.complete) { p.classList.add("done") }
+    //     p.addEventListener("click", (e) => { e.target.classList.toggle("done"); this.isChecked(got.id) })
+    //   })
+    // }
+  }
 };
 </script>
 
@@ -41,6 +82,7 @@ body {
   text-align: center;
   box-sizing: border-box;
 }
+
 #background {
   width: 100%;
   height: 100vh;
@@ -54,18 +96,21 @@ body {
 #main {
   width: 40%;
   height: 50%;
+  display: inline-block;
   margin: auto;
   margin-top: 250px;
   border: 2px solid black;
   border-radius: 15px;
   background: white;
 }
+
 #title {
   font-size: 3rem;
   margin: auto;
   margin-top: 10px;
   margin-bottom: 20px;
 }
+
 #title:hover {
   color: chocolate;
 }
@@ -89,5 +134,9 @@ body {
   margin-left: 5px;
   background: darkorange;
   cursor: pointer;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
--- todo/src/components/List.vue
+++ todo/src/components/List.vue
@@ -1,34 +1,49 @@
 <template>
-  <hr style="border:1px dashed black"/>
-  <div class="list">
-    <input class="checkbox" type="checkbox" />
-    <p class="inputData">input에서 받은 데이터</p>
-    <button class="del">X</button><br />
+  <div>
+    <div id="todo-list">
+      <input v-model="state" class="checkbox" type="checkbox" />
+      <p class="inputData">{{loadData.data}}</p>
+      <button class="del" @click="$emit('tog')">X</button><br />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "List",
+  data() {
+    return {
+      state: false
+    }
+  },
+  props: {
+    loadData: Array
+  },
+  methods: {
+
+  }
 };
 </script>
 
 <style>
-.list {
+#todo-list {
   display: flex;
   width: 80%;
   margin: auto;
   margin-top: 20px;
 }
+
 .checkbox {
   flex-grow: 1;
   cursor: pointer;
 }
+
 .inputData {
   font-size: 1.5rem;
   flex-grow: 7;
   margin: 0;
 }
+
 .del {
   font-size: 1.5rem;
   border: none;
