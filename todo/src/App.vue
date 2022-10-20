c4c65cd7bc9b05a201c2c3d6ab5764b47d902e11
Jeong Jiun <jujeong@ncurity.com>
2022-10-20 15:53:05 +0900

    finalTodo

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <!-- 남은사항
-체크박스 체크시 complete값 제대로 변경(LS에 상태저장) + 가운뎃줄(LS에 상태저장)
-개별삭제,선택삭제,진행도그래프 -->
+  <!-- 남은사항 : 진행도그래프 -->
   <div id="background">
     <div id="main">
       <div>
@@ -22,7 +20,7 @@
 <script>
 //input부분을 컴포넌트로 만들고 이점보다 데이터를 주고받을때의 복잡함 등 단점이 더 크다고 판단합니다.
 //props로 보내면 자식컴포넌트에서는 Read-Only가 되기때문에 일반적으로 변경이 되지않고, 설령 Custom Event를 사용하더라도 코드가 더 복잡해지는 경우가 생겨 input부분은 컴포넌트로 사용하지 않았습니다.
-//컴포넌트는 반복적인 구문이 올때 효과적이며 간결하게 보일수 있지만, '한번만 쓰이고 데이터를 양방향으로 보내는 블럭'에 사용하기에는 오히려 부적절(복잡해) 합니다.(강의보고 조금더 보충하자)
+//컴포넌트는 반복적인 구문이 올때 효과적이며 간결하게 보일수 있지만, '한번만 쓰이고 데이터를 양방향으로 보내는 블럭'에 사용하기에는 오히려 부적절(복잡해) 합니다.
 import List from "./components/List.vue";
 
 export default {
@@ -44,14 +42,14 @@ export default {
         return this.get;
       }
     },
-
+    //입력데이터 저장
     setData: function () {
-      if (this.Inputs == "") {
+      if (this.Inputs == "") { //미입력시 alert
         alert("할일을 입력하세요.");
         return;
       }
       this.addData(this.Inputs);
-      this.Inputs = "";
+      this.Inputs = ""; //입력후 입력창 초기화
     },
     addData: function (inputText) {
       this.get.push({
@@ -64,28 +62,30 @@ export default {
 
     //1번째(List.vue) : 클릭을 하면 해당된id를 보내서 isChecked파라미터로 전송한다.
     //2번째(isChecked) : loadData()에서 for문을쓰든 filter를쓰든 ★id를 비교하여 해당된것의 complete값을 반대로 바꾼후 localStorage에 다시 저장한다.
+    //완료처리
     isChecked: function (id) {
-      const checking = this.get.filter(C => C.id == id)[0]; //filter는 Array를 리턴한다!
+      const checking = this.get.filter(C => C.id == id)[0]; //filter는 Array를 리턴한다! - 하나라도 첫번째 항목지정해야
       checking.complete = !checking.complete;
       localStorage.setItem("list", JSON.stringify(this.get));
       this.get = JSON.parse(localStorage.getItem('list')) //데이터를 다시 변수에 넣어야 실시간렌더링 됌 loadData가 리턴값을 가지는데 변수에다 함수를 실행하면 오류가나기때문에 그냥 this.get적음
     },
+    //전체삭제
     delAll: function () {
       localStorage.removeItem("list")
       this.get = JSON.parse(localStorage.getItem('list'))
     },
+    //체크된것 삭제
     delSel: function () {
-      //complete가 false인 애들을 변수에 담고 setItem
-      const arr = this.get.filter(C => C.complete != true);
+      const arr = this.get.filter(C => C.complete != true); //complete값 비교하여 false인것만 다시 담아서 저장
       localStorage.setItem("list", JSON.stringify(arr));
       this.get = JSON.parse(localStorage.getItem('list'))
     },
+    //개별항목 삭제
     del: function (id) {
-      const del = this.get.filter(C => C.id != id);
+      const del = this.get.filter(C => C.id != id); //선택한것과 다른 항목들만 다시 담아서 저장
       localStorage.setItem("list", JSON.stringify(del));
       this.get = JSON.parse(localStorage.getItem('list'))
     }
-
   },
 };
 </script>
